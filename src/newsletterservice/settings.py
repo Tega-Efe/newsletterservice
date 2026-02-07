0195747fb1be83998a071a6a1fd6ca7ec6849018
Tega-Efe <146211433+Tega-Efe@users.noreply.github.com>
2026-02-07 23:56:20 +0100

    newsletterservice hosted

--- src/newsletterservice/settings.py
+++ src/newsletterservice/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-drih5ul)03h%b6wtmzhtn)w3px_zeh$bls3cb5x#)y!ozgu&)z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','10.11.196.109','172.20.10.3' ,'newsletterservice.pythonanywhere.com' ]
 
